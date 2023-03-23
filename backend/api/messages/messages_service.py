e8e31a0506bf9d45b845280768a16055079bfd8b
leopapadopoulos6 <leopapadopoulos6@gmail.com>
2023-03-23 15:39:17 -0400

    CourseReview

--- backend/api/messages/messages_service.py
+++ backend/api/messages/messages_service.py
@@ -1,4 +1,5 @@
 from api.messages.message import Message
+from api.messages.message import CourseReview
 
 
 def get_public_message():
@@ -17,3 +18,20 @@ def get_admin_message():
     return Message(
         "This is an admin message."
     )
+
+
+def submit_review(data):
+    data = request.get_json()
+
+    course_review = CourseReview()
+    course_review.create_review(
+    reviewer=data['reviewer'],
+    coursename=data['course_name'],
+    criteria=[data['difficulty'], data['interest'], data['usefulness'], data['organization'], data['workload'], data['rating']],
+    code=data['course_code'],
+    text=data['review_text'],
+    term=data['term'],
+    year=data['year']
+)
+    
+    return jsonify({'message': 'Review submitted successfully'}), 201
