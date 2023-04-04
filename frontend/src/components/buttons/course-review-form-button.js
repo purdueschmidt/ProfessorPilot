91ed401117710c5885309feeedc7ba00c9003d06
leopapadopoulos6 <leopapadopoulos6@gmail.com>
2023-04-04 03:53:22 -0400

    course review button added in navbar, removed from pages

---
+++ frontend/src/components/buttons/course-review-form-button.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { ReviewForm } from "../review-form/review-form";
+import "../../styles/components/button.css"
+
+const ReviewFormButton = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button className="button__review-form-button" onClick={handleClickOpen} color="inherit">
+        Review Course
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Course Review Form</DialogTitle>
+        <DialogContent>
+          <ReviewForm handleClose={handleClose} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default ReviewFormButton;
