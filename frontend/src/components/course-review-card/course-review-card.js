b7689e83cfb33c5b2c6707ace62e4a98af6d8b67
leopapadopoulos6 <leopapadopoulos6@gmail.com>
2023-04-13 17:26:36 -0400

    COURSE REVIEWS- reviewer: grabs the user nickname, rating: averages the values, up/down vote: check if logged in & show updated

--- frontend/src/components/course-review-card/course-review-card.js
+++ frontend/src/components/course-review-card/course-review-card.js
@@ -17,6 +17,7 @@ export const CourseReviewCard = ({ _id, term, year, rating, reviewer, course_cod
     const [upVotes, setUpVotes] = useState(UpVotes);
     const [downVotes, setDownVotes] = useState(DownVotes);
     const { user } = useAuth0
+    const { isAuthenticated } = useAuth0()
   
     useEffect(() => {
         setUpVotes(UpVotes);
@@ -31,55 +32,142 @@ export const CourseReviewCard = ({ _id, term, year, rating, reviewer, course_cod
         onVote(_id, 'downvote');
       };
       
-      
-      
-
+    
     return (
-    <Card className='card' sx={{ minWidth: 200, mt: 2, mb: 2 }}>
-        <CardContent className='card-content'>
-        <Typography variant="h4" align="center" className="review-form-title" gutterBottom>Reviewer: {reviewer}</Typography>
-            <Grid container>
-                <Grid item xs={12} md={1}>
-                    <Box className='rating-box'>
-                        
-                        <IconButton className='upvote-button' onClick={handleUpvote}>
-                            <ThumbUpIcon />
-                        </IconButton>
-                        <Typography className='text' variant="subtitle1">{upVotes}</Typography>
-                        <IconButton className='downvote-button' onClick={handleDownvote}>
-                            <ThumbDownIcon />
-                        </IconButton>
-                        <Typography className='text' variant="subtitle1">{downVotes}</Typography>
-                    </Box>
-                </Grid>
-
-                <Grid item xs={12} md={3}>
-                    <Box className='rating-box'>
-                        <Box className='rating'>
-                            <Typography className='text' variant="h6">Rating {rating}</Typography>
-                        </Box>
-                        <Typography className='text' variant="subtitle1">Workload {workload}</Typography>
-                        <Typography className='text' variant="subtitle1">Organization {organization}</Typography>
-                        <Typography className='text' variant="subtitle1">Usefulness {usefulness}</Typography>
-                        <Typography className='text' variant="subtitle1">Interest {interest}</Typography>
-                        <Typography className='text' variant="subtitle1">Difficulty {difficulty}</Typography>
-                    </Box>
-                </Grid>
-                <Divider orientation="vertical" flexItem />
-                <Grid item xs={12} md={2}>
-                    <Box className='info-box' ml={1}>
-                        {/* <Typography className='text' variant="h6">{courseName}</Typography> */}
+    <div>
+        {!isAuthenticated && (
+        <>
+            <Card className='card' sx={{ minWidth: 200, mt: 2, mb: 2 }}>
+                <CardContent className='card-content'>
+                <Typography variant="h4" align="center" className="review-form-title" gutterBottom>Reviewer: {reviewer}</Typography>
+                    <Grid container>
+                        <Grid item xs={12} md={3}>
+                            <Box className='rating-box'>
+                                <Box className='rating'>
+                                    <Typography className='text' variant="h6">Rating {rating}</Typography>
+                                </Box>
+                                <Typography className='text' variant="subtitle1">Workload {workload}</Typography>
+                                <Typography className='text' variant="subtitle1">Organization {organization}</Typography>
+                                <Typography className='text' variant="subtitle1">Usefulness {usefulness}</Typography>
+                                <Typography className='text' variant="subtitle1">Interest {interest}</Typography>
+                                <Typography className='text' variant="subtitle1">Difficulty {difficulty}</Typography>
+                            </Box>
+                        </Grid>
                         <Divider orientation="vertical" flexItem />
-                        <Typography className='text' variant="subtitle1">{term}, {year}</Typography>
-                        <Typography className='text' variant="subtitle1">Course Code: {course_code}</Typography>
-                        <Box className='review-box' mt={1}>
-                            <Typography className='text'>{reviewText}</Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </CardContent>
-    </Card>
+                        <Grid item xs={12} md={2}>
+                            <Box className='info-box' ml={1}>
+                                {/* <Typography className='text' variant="h6">{courseName}</Typography> */}
+                                <Divider orientation="vertical" flexItem />
+                                <Typography className='text' variant="subtitle1">{term}, {year}</Typography>
+                                <Typography className='text' variant="subtitle1">Course Code: {course_code}</Typography>
+                                <Box className='review-box' mt={1}>
+                                    <Typography className='text'>{reviewText}</Typography>
+                                </Box>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+        </>
+        )}
+        {isAuthenticated && (
+        <>
+            <Card className='card' sx={{ minWidth: 200, mt: 2, mb: 2 }}>
+                <CardContent className='card-content'>
+                <Typography variant="h4" align="center" className="review-form-title" gutterBottom>Reviewer: {reviewer}</Typography>
+                    <Grid container>
+                        
+                        <Grid item xs={12} md={1}>
+                            <Box className='rating-box'>
+                                
+                                <IconButton className='upvote-button' onClick={handleUpvote}>
+                                    <ThumbUpIcon />
+                                </IconButton>
+                                <Typography className='text' variant="subtitle1">{upVotes}</Typography>
+                                <IconButton className='downvote-button' onClick={handleDownvote}>
+                                    <ThumbDownIcon />
+                                </IconButton>
+                                <Typography className='text' variant="subtitle1">{downVotes}</Typography>
+                            </Box>
+                        </Grid>
+
+                        <Grid item xs={12} md={3}>
+                            <Box className='rating-box'>
+                                <Box className='rating'>
+                                    <Typography className='text' variant="h6">Rating {rating}</Typography>
+                                </Box>
+                                <Typography className='text' variant="subtitle1">Workload {workload}</Typography>
+                                <Typography className='text' variant="subtitle1">Organization {organization}</Typography>
+                                <Typography className='text' variant="subtitle1">Usefulness {usefulness}</Typography>
+                                <Typography className='text' variant="subtitle1">Interest {interest}</Typography>
+                                <Typography className='text' variant="subtitle1">Difficulty {difficulty}</Typography>
+                            </Box>
+                        </Grid>
+                        <Divider orientation="vertical" flexItem />
+                        <Grid item xs={12} md={2}>
+                            <Box className='info-box' ml={1}>
+                                {/* <Typography className='text' variant="h6">{courseName}</Typography> */}
+                                <Divider orientation="vertical" flexItem />
+                                <Typography className='text' variant="subtitle1">{term}, {year}</Typography>
+                                <Typography className='text' variant="subtitle1">Course Code: {course_code}</Typography>
+                                <Box className='review-box' mt={1}>
+                                    <Typography className='text'>{reviewText}</Typography>
+                                </Box>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+        </>
+        )}
+    </div>
+
+    // <Card className='card' sx={{ minWidth: 200, mt: 2, mb: 2 }}>
+    //     <CardContent className='card-content'>
+    //     <Typography variant="h4" align="center" className="review-form-title" gutterBottom>Reviewer: {reviewer}</Typography>
+    //         <Grid container>
+                
+    //             <Grid item xs={12} md={1}>
+    //                 <Box className='rating-box'>
+                        
+    //                     <IconButton className='upvote-button' onClick={handleUpvote}>
+    //                         <ThumbUpIcon />
+    //                     </IconButton>
+    //                     <Typography className='text' variant="subtitle1">{upVotes}</Typography>
+    //                     <IconButton className='downvote-button' onClick={handleDownvote}>
+    //                         <ThumbDownIcon />
+    //                     </IconButton>
+    //                     <Typography className='text' variant="subtitle1">{downVotes}</Typography>
+    //                 </Box>
+    //             </Grid>
+
+    //             <Grid item xs={12} md={3}>
+    //                 <Box className='rating-box'>
+    //                     <Box className='rating'>
+    //                         <Typography className='text' variant="h6">Rating {rating}</Typography>
+    //                     </Box>
+    //                     <Typography className='text' variant="subtitle1">Workload {workload}</Typography>
+    //                     <Typography className='text' variant="subtitle1">Organization {organization}</Typography>
+    //                     <Typography className='text' variant="subtitle1">Usefulness {usefulness}</Typography>
+    //                     <Typography className='text' variant="subtitle1">Interest {interest}</Typography>
+    //                     <Typography className='text' variant="subtitle1">Difficulty {difficulty}</Typography>
+    //                 </Box>
+    //             </Grid>
+    //             <Divider orientation="vertical" flexItem />
+    //             <Grid item xs={12} md={2}>
+    //                 <Box className='info-box' ml={1}>
+    //                     {/* <Typography className='text' variant="h6">{courseName}</Typography> */}
+    //                     <Divider orientation="vertical" flexItem />
+    //                     <Typography className='text' variant="subtitle1">{term}, {year}</Typography>
+    //                     <Typography className='text' variant="subtitle1">Course Code: {course_code}</Typography>
+    //                     <Box className='review-box' mt={1}>
+    //                         <Typography className='text'>{reviewText}</Typography>
+    //                     </Box>
+    //                 </Box>
+    //             </Grid>
+    //         </Grid>
+    //     </CardContent>
+    // </Card>
   );
 };
 
