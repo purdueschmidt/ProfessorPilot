3b7c624064831471f3f3268cae21b244c895cc0d
Justin Schmidt <justin@linux-bqkc.home>
2023-04-24 15:27:17 -0400

    merge fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,40 +12,13 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 const theme = createTheme({
-    palette: {
-        mode: 'light',
-        primary: {
-            main: '#003831',
-            light: '#2a5c4b',
-            dark: '#00201f',
-        },
-        secondary: {
-            main: '#efb21e',
-            light: '#ffca4b',
-            dark: '#c58600',
-        },
-        background: {
-            default: 'rgba(234,234,197,0.75)',
-            paper: '#eaeac5',
-        },
-        info: {
-            main: '#1976d2',
-            light: '#63a4ff',
-            dark: '#004ba0',
-        },
-        divider: '#9c824a',
-        text: {
-            primary: '#0c0c0c',
-            secondary: '#616161',
-            disabled: '#9e9e9e',
-        },
-        warning: {
-            main: '#ffa000',
-            dark: '#c67100',
-            light: '#ffd149',
-        },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#003831',
+      light: '#2a5c4b',
+      dark: '#00201f',
     },
-    
     secondary: {
       main: '#efb21e',
       light: '#ffca4b',
